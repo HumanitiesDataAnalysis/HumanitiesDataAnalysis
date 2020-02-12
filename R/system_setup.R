ba04930c3bbd16530664abca8f64a8ba783f278c
Benjamin Schmidt <bmschmidt@gmail.com>
2020-02-11 22:34:11 -0500

    Adding function to download problem sets.

--- R/system_setup.R
+++ R/system_setup.R
@@ -3,3 +3,30 @@ install_custom_markdown_template = function () {
   if (!dir.exists("~/.config/rstudio/templates")) dir.create("~/.config/rstudio/templates", recursive = TRUE)
   file.copy(system.file("extdata", "default.Rmd", package = "HumanitiesDataAnalysis"), "~/.config/rstudio/templates")
 }
+
+
+#' Download problem sets
+#'
+#' Download all problem sets into the current direct as .Rmd files.
+#'
+#' @return Nothing
+#' @export
+#'
+download_problem_sets = function() {
+  req <- httr::GET("https://api.github.com/repos/HumanitiesDataAnalysis/book/git/trees/master?recursive=1")
+  stop_for_status(req)
+  filelist <- unlist(lapply(content(req)$tree, "[", "path"), use.names = F)
+  sets = filelist[grepl("problem.*.Rmd", filelist)]
+  downloads = FALSE
+  lapply(sets, function(set) {
+    if (!file.exists(set)) {
+      downloads <<- TRUE
+      url = stringr::str_glue("https://raw.githubusercontent.com/HumanitiesDataAnalysis/book/master/{set}")
+      message(stringr::str_glue("Downloading {set} to current directory."))
+      download.file(url, set, quiet = TRUE)
+    }
+  })
+  if (downloads == FALSE) {
+    message("You've downloaded all the problem sets already.")
+  }
+}
