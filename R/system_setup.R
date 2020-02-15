68acd62966a93d5df25276fe5e872cfe882c600f
Ben Schmidt <bmschmidt@gmail.com>
2020-02-14 23:43:29 -0500

    Download sets from course site; KEEP_GOING function.

--- R/system_setup.R
+++ R/system_setup.R
@@ -4,6 +4,24 @@ install_custom_markdown_template = function () {
   file.copy(system.file("extdata", "default.Rmd", package = "HumanitiesDataAnalysis"), "~/.config/rstudio/templates")
 }
 
+#' Code placeholder
+#'
+#' A placeholder in problem sets that swallows up data.
+#'
+#' @param ...
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
+#' KEEP_GOING("This is discarded")
+#'
+KEEP_GOING = function(...) {
+  if (interactive()) {
+    message("You need to replace the part that says KEEP_GOING with more code!")
+  }
+}
 
 #' Download problem sets
 #'
@@ -12,16 +30,25 @@ install_custom_markdown_template = function () {
 #' @return Nothing
 #' @export
 #'
-download_problem_sets = function() {
-  req <- httr::GET("https://api.github.com/repos/HumanitiesDataAnalysis/book/git/trees/master?recursive=1")
-  httr::stop_for_status(req)
-  filelist <- unlist(lapply(httr::content(req)$tree, "[", "path"), use.names = F)
+download_problem_sets = function(from = "HDA") {
+
+  if (from == 'github') {
+    req <- httr::GET("https://api.github.com/repos/HumanitiesDataAnalysis/book/git/trees/master?recursive=1")
+    httr::stop_for_status(req)
+    filelist <- unlist(lapply(httr::content(req)$tree, "[", "path"), use.names = F)
+  } else {
+    sets = readr::read_lines("http://benschmidt.org/HDA/extracted_problem_sets/manifest.txt")
+  }
   sets = filelist[grepl("extracted_problem.*.Rmd", filelist)]
   downloads = FALSE
   lapply(sets, function(set) {
     if (!file.exists(set)) {
       downloads <<- TRUE
-      url = stringr::str_glue("https://raw.githubusercontent.com/HumanitiesDataAnalysis/book/master/{set}")
+      if (from == "github") {
+        url = stringr::str_glue("https://raw.githubusercontent.com/HumanitiesDataAnalysis/book/master/{set}")
+      } else {
+        url = stringr::str_glue("http://benschmidt.org/HDA/extracted_problem_sets/{set}")
+      }
       message(stringr::str_glue("Downloading {set} to current directory."))
       set = str_replace(set, ".*/", "")
       download.file(url, set, quiet = TRUE)
